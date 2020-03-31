e0359555fcb798bd767101609990064a26ea7597
Dominik-Hillmann <dominik.hillmann@gmx.de>
2020-03-31 23:22:02 +0200

    Almost finished first model

--- main_2020_03_31_1.py
+++ main_2020_03_31_1.py
@@ -1,5 +1,29 @@
-from util.get_128px_data import get_128px_test_data, get_128px_train_data
+# Python libraries
+import os
 
-print(__doc__)
-print(get_128px_train_data())
-print(get_128px_test_data())
+# Internal modules
+from util.get_128px_data import get_128px_test_data, get_128px_train_data
+from util.use_gpu import detect_gpu, get_device
+from modelling.model_2020_03_31_1.ConvolutionalNeuralNet import ConvolutionalNeuralNet
+from util.PerformanceTracker import PerformanceTracker
+
+
+def main() -> None:
+    detect_gpu()
+    device = get_device()
+
+    model = ConvolutionalNeuralNet((64, 32, 16), (1024, 4), device)
+    train_X, train_y, val_X, val_y = get_128px_train_data()
+    tracker = PerformanceTracker(os.path.join('modelling', 'model_2020_03_31_1'))
+
+    model.train(
+        (train_X, train_y), 
+        (val_X, val_y), 
+        device, tracker, 
+        2, 
+        10
+    )
+
+
+if __name__ == '__main__':
+    main()
--- util/get_128px_data.py
+++ util/get_128px_data.py
@@ -4,23 +4,57 @@ import csv
 
 # External modules
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# Constants
+SEED = 69
+
+# Typing
+from typing import Tuple
 
 
-def get_128px_train_data():
+def get_128px_train_data(val_size: float = 0.2) -> Tuple[
+    np.array,
+    np.array,
+    np.array,
+    np.array
+]:
     imgs_path = os.path.abspath(os.path.join('data', 'generated-data', 'train-128.csv'))
     train_X = pd.read_csv(imgs_path, quoting = csv.QUOTE_MINIMAL, index_col = 0)
 
     labels_path = os.path.join('data', 'raw-data', 'train.csv')
     train_y = pd.read_csv(labels_path, quoting = csv.QUOTE_MINIMAL)
 
-    return train_X, train_y
+    train_X.index = train_X.image_id
+    train_X = train_X.sort_index()
+    train_X = train_X.drop('image_id', axis = 'columns')
+    train_y.index = train_y.image_id
+    train_y = train_y.sort_index()
+    train_y = train_y.drop('image_id', axis = 'columns')
+
+    train_X, val_X, train_y, val_y = train_test_split(
+        train_X, 
+        train_y, 
+        test_size = val_size,
+        random_state = SEED
+    )
+
+    return train_X.values, train_y.values, val_X.values, val_y.values
 
 
-def get_128px_test_data():
+def get_128px_test_data() -> Tuple[np.array, np.array]:
     imgs_path = os.path.join('data', 'generated-data', 'test-128.csv')
     test_X = pd.read_csv(imgs_path, quoting = csv.QUOTE_MINIMAL, index_col = 0)
 
     labels_path = os.path.join('data', 'raw-data', 'test.csv')
     test_y = pd.read_csv(labels_path, quoting = csv.QUOTE_MINIMAL)
 
-    return test_X, test_y
+    test_X.index = test_X.image_id
+    test_X = test_X.sort_index()
+    test_X = test_X.drop('image_id', axis = 'columns')
+    test_y.index = test_y.image_id
+    test_y = test_y.sort_index()
+    test_y = test_y.drop('image_id', axis = 'columns')
+
+    return test_X.values, test_y.values
