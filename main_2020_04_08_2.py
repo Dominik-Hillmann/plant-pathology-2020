72cb086574087866fda9c4ddcb2b48a8a688ed5f
Dominik-Hillmann <dominik.hillmann@gmx.de>
2020-04-24 23:40:47 +0200

    New model with less neurons in last layer

--- main_2020_04_08_2.py
+++ main_2020_04_08_2.py
@@ -12,9 +12,9 @@ from util.create_submit import create_submit
 def main_train() -> None:
     detect_gpu()
     device = get_device()
-    save_dir = path.join('modelling', 'model_2020_04_08_1')
+    save_dir = path.join('modelling', 'model_2020_04_08_2')
 
-    model = CNNAugDataRegularized((64, 64, 128, 256), (1024, 256), 4, device)
+    model = CNNAugDataRegularized((32, 64, 128, 256), (512, 128), 4, device)
     data = FirstAugmentedDataset()
     tracker = PerformanceTracker(save_dir)
     model.train(data, 60, 20, tracker, learning_rate = 0.0001)
@@ -23,22 +23,21 @@ def main_train() -> None:
     tracker.save('metrics.csv')
 
 
-# def main_predict() -> None:
-#     detect_gpu()
-#     device = get_device()
-#     save_dir = path.join('modelling', 'model_2020_04_08_1')
+def main_predict() -> None:
+    detect_gpu()
+    device = get_device()
+    save_dir = path.join('modelling', 'model_2020_04_08_2')
 
-#     model = CNNAugDataRegularized((32, 64, 128, 256), (1024, 128), 4, device)
-#     data = FirstAugmentedDataset()
-#     tracker = PerformanceTracker(save_dir)
-#     # Number if epochs determined by minimum loss of in training.
-#     model.train(data, 13, 20, tracker, learning_rate = 0.0001)
+    model = CNNAugDataRegularized((32, 64, 128, 256), (512, 128), 4, device)
+    data = FirstAugmentedDataset()
+    tracker = PerformanceTracker(save_dir)
+    model.train(data, 18, 20, tracker, learning_rate = 0.0001)
     
-#     test_X, imgs_ids = data.get_test()
-#     pred_y = model.predict(test_X)
-#     create_submit(pred_y, imgs_ids, path.join(save_dir, 'submission.csv'))
+    test_X, imgs_ids = data.get_test()
+    pred_y = model.predict(test_X)
+    create_submit(pred_y, imgs_ids, path.join(save_dir, 'submission.csv'))
 
 
 if __name__ == '__main__':
-    main_train()
-    # main_predict()
+    # main_train()
+    main_predict()
