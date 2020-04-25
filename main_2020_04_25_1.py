9767c3eb37ccc28ffc695f1a0b0b6bc0da071a76
Dominik-Hillmann <dominik.hillmann@gmx.de>
2020-04-25 23:54:34 +0200

    Dropout model

---
+++ main_2020_04_25_1.py
@@ -0,0 +1,50 @@
+# Python libraries
+from os import path
+
+# Internal modules
+from util.datasets import FirstAugmentedDataset
+from util.use_gpu import detect_gpu, get_device
+from modelling.model_2020_04_25_1.DropoutCNN import DropoutCNN
+from util.PerformanceTracker import PerformanceTracker
+from util.create_submit import create_submit
+
+
+def main_train() -> None:
+    detect_gpu()
+    device = get_device()
+    save_dir = path.join('modelling', 'model_2020_04_25_1')
+
+    model = DropoutCNN(
+        (32, 64, 128, 256), 
+        (512, 128), 
+        4, 
+        device,
+        drop_dense_p = 0.2,
+        drop_conv_p = 0.2
+    )
+    data = FirstAugmentedDataset()
+    tracker = PerformanceTracker(save_dir)
+    model.train(data, 40, 20, tracker, learning_rate = 0.0001)
+    
+    tracker.graphs()
+    tracker.save('metrics.csv')
+
+
+def main_predict() -> None:
+    detect_gpu()
+    device = get_device()
+    save_dir = path.join('modelling', 'model_2020_04_08_2')
+
+    model = CNNAugDataRegularized((32, 64, 128, 256), (512, 128), 4, device)
+    data = FirstAugmentedDataset()
+    tracker = PerformanceTracker(save_dir)
+    model.train(data, 18, 20, tracker, learning_rate = 0.0001)
+    
+    test_X, imgs_ids = data.get_test()
+    pred_y = model.predict(test_X)
+    create_submit(pred_y, imgs_ids, path.join(save_dir, 'submission.csv'))
+
+
+if __name__ == '__main__':
+    main_train()
+    # main_predict()
