b30c336a62f574dcf3dc9317a6fd813c9aa658d5
Dominik-Hillmann <dominik.hillmann@gmx.de>
2020-04-29 22:04:55 +0200

    Implemented stride instead of max pooling

---
+++ main_2020_04_29_1.py
@@ -0,0 +1,43 @@
+# Python libraries
+from os import path
+
+# Internal modules
+from util.datasets import FirstAugmentedDataset
+from util.use_gpu import detect_gpu, get_device
+from modelling.model_2020_04_29_1.StrideCNN import StrideCNN
+from util.PerformanceTracker import PerformanceTracker
+from util.create_submit import create_submit
+
+
+def main_train() -> None:
+    detect_gpu()
+    device = get_device()
+    save_dir = path.join('modelling', 'model_2020_04_29_1')
+
+    model = StrideCNN((16, 32, 64, 256), (512, 128), 4, device)
+    data = FirstAugmentedDataset()
+    tracker = PerformanceTracker(save_dir)
+    model.train(data, 60, 16, tracker, learning_rate = 0.0001)
+    
+    tracker.graphs()
+    tracker.save('metrics.csv')
+
+
+# def main_predict() -> None:
+#     detect_gpu()
+#     device = get_device()
+#     save_dir = path.join('modelling', 'model_2020_04_29_1')
+
+#     model = CNNAugDataRegularized((16, 32, 64, 256), (512, 128), 4, device)
+#     data = FirstAugmentedDataset()
+#     tracker = PerformanceTracker(save_dir)
+#     model.train(data, 18, 20, tracker, learning_rate = 0.001)
+    
+#     test_X, imgs_ids = data.get_test()
+#     pred_y = model.predict(test_X)
+#     create_submit(pred_y, imgs_ids, path.join(save_dir, 'submission.csv'))
+
+
+if __name__ == '__main__':
+    main_train()
+    # main_predict()
--- modelling/model_2020_04_29_1/StrideCNN.py
+++ modelling/model_2020_04_29_1/StrideCNN.py
@@ -7,7 +7,6 @@ import torch
 from torch import nn
 import torch.nn.functional as func
 import torch.optim as optim
-from torchvision.models.resnet import resnet18
 
 import pandas as pd
 import numpy as np
@@ -20,13 +19,17 @@ from util.datasets import Dataset
 # Typing
 from typing import Tuple
 
-# https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
-class TransferCNN(nn.Module):
+
+class StrideCNN(nn.Module):
+    """Tale the best model so far and use stride to lower resolution instead
+    of MaxPooling since this is an outdated method."""
 
     def __init__(
         self,
+        num_filters: Tuple[int, int, int, int],
+        num_neurons_in_layers: Tuple[int, int],
+        num_y_classes: int,
         device: torch.cuda.device,
-        num_y_classes: int = 4,
         loss_function = nn.CrossEntropyLoss(),
         last_layer_activation = nn.Softmax(dim = 1)
     ):
@@ -35,18 +38,59 @@ class TransferCNN(nn.Module):
         self.activation = last_layer_activation
         self.used_device = device
 
-        self.base = resnet18(pretrained = True)
-        # for param in self.base.parameters():
-        #     param.requires_grad = False
+        num_1st_filter, num_2nd_filter, num_3rd_filter, num_4th_filter = num_filters
+        num_1st_layer_neurons, num_2nd_layer_neurons = num_neurons_in_layers
         
-        last_layer_input = self.base.fc.in_features
-        self.base.fc = nn.Linear(last_layer_input, 128)
-        self.last = nn.Linear(128, num_y_classes)
+
+        self.conv1 = nn.Conv2d(3, num_1st_filter, 7, stride = 2)
+        self.conv2 = nn.Conv2d(num_1st_filter, num_2nd_filter, 3, stride = 2)
+        self.conv3 = nn.Conv2d(num_2nd_filter, num_3rd_filter, 3, stride = 2)
+        self.conv4 = nn.Conv2d(num_3rd_filter, num_4th_filter, 3, stride = 2)
+
+        # First dense input = [batch_size, height * width * num_channels]
+        x = torch.randn(3, 128, 128).view(-1, 3, 128, 128)
+        self._conv_out_len = None
+        self._conv_forward(x)
+
+        self.dense1 = nn.Linear(self._conv_out_len, num_1st_layer_neurons) 
+        self.dense2 = nn.Linear(num_1st_layer_neurons, num_2nd_layer_neurons) 
+        self.dense3 = nn.Linear(num_2nd_layer_neurons, num_y_classes)
+
+
+    def _conv_forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = func.relu(self.conv1(x))
+        x = func.relu(self.conv2(x))
+        x = func.relu(self.conv3(x))
+        x = func.relu(self.conv4(x))
+
+        if self._conv_out_len is None:
+            print('In:', x.shape)
+            num_features = x[0].shape[0]
+            num_px_height = x[0].shape[1]
+            num_px_width = x[0].shape[2]
+            self._conv_out_len = num_features * num_px_height * num_px_width
+            print('Out:', num_features * num_px_height * num_px_width)
+
+        return x
+
+    
+    def _prepare_conv_to_dense(self, x: torch.Tensor) -> torch.Tensor:
+        return x.view(-1, self._conv_out_len)
+    
+
+    def _dense_forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = func.relu(self.dense1(x))
+        x = func.relu(self.dense2(x))
+        x = func.relu(self.dense3(x))
+
+        return x
 
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.base(x)
-        x = self.last(x)
+        x = self._conv_forward(x)
+        x = self._prepare_conv_to_dense(x)
+        x = self._dense_forward(x)
+
         return x
 
 
